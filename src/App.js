f38b64d519b9d2f038b7aa45182fa4d2e12409c8
Amelia Schulz <you@example.com>
2018-12-03 10:39:22 -0500

    renders names and scores to screen

--- src/App.js
+++ src/App.js
@@ -3,25 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    // set up state
+    this.state = {
+      scores: [
+        
+        {
+          id: 1,
+          name: 'amelia',
+          score: 1001
+        },
+
+        {
+          id: 2,
+          name: 'stacey',
+          score: 999
+        },
+
+        {
+          id: 3,
+          name: 'sasha',
+          score: 500
+        }
+      ]
+    }
+  
+  
+  
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>SCoReBoArD App</h1>
+        <div className="score-container">
+        {this._scoresAsCards()}
+        </div>
       </div>
     );
+  }
+
+  _scoresAsCards() {
+    const cards = this.state.scores.map(score => {
+      return (
+        <div>
+          <h2>Name:{score.name}// Score: {score.score}</h2>
+        </div>
+      )
+    });
+    return cards;
   }
 }
 
