7b1e1beedb9bbb3c7d33edb5d997de920d329091
Amelia Schulz <you@example.com>
2018-12-03 11:09:28 -0500

    adds increment score function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -46,12 +45,35 @@ class App extends Component {
   _scoresAsCards() {
     const cards = this.state.scores.map(score => {
       return (
-        <div>
-          <h2>Name:{score.name}// Score: {score.score}</h2>
+        <div key={score.id}>
+          <h2>Name:{score.name} // Score: {score.score}</h2>
+          <button onClick={()=> this._incrementScoreById(score.id)}>+</button>
         </div>
       )
     });
     return cards;
+  }
+
+
+  _incrementScoreById(id){
+    console.log('i heaaaaar you')
+    // find the player in this.state.scores
+    // increment their scores
+    const newScores = this.state.scores.map(jeff => {
+      if(jeff.id !== id ){
+        return jeff
+      } else {
+        return {
+          id: jeff.id,
+          name: jeff.name,
+          score: jeff.score + 1
+        }
+      }
+    })
+    // and call this.setState
+      this.setState({
+        scores: newScores
+      });
   }
 }
 
